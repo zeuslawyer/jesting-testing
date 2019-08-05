de8ffb2a639d04efe5a0829587355f08a382d6de
zeuslawyer <zubin.pratap@gmail.com>
2019-08-05 12:04:14 +1000

    removing ReactDOm import from app.test.  see if codesandbox auto updates...

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,6 +1,8 @@
-import React from "react";
-import { mount } from "enzyme";
-import CommentBox from "../CommentBox";
+// @ts-nocheck
+
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from '../CommentBox';
 
 /*
     this test does not use enzyme's shallow DOM render.
@@ -12,42 +14,42 @@ let wrapped;
 beforeEach(setup);
 function setup() {
   wrapped = mount(<CommentBox />);
-  console.log("***Setup completed***");
+  console.log('***Setup completed***');
 }
 
 // CLEANUP and UNMOUNT as mount() = FULLDOM render
 afterEach(cleanup);
 function cleanup() {
   wrapped.unmount();
-  console.log("***Unmounted.***");
+  console.log('***Unmounted.***');
 }
 
 //TESTS
-describe("tests the CommentBox and Its Children", () => {
+describe('tests the CommentBox and Its Children', () => {
   // test 1
-  it("has a textArea and a button ", () => {
-    const textareas = wrapped.find("textarea");
-    const buttons = wrapped.find("button");
+  it('has a textArea and a button ', () => {
+    const textareas = wrapped.find('textarea');
+    const buttons = wrapped.find('button');
     expect(textareas.length).toEqual(1);
     expect(buttons.length).toEqual(1);
   });
 
-  it("has a text areas that users can type into ", () => {
+  it('has a text areas that users can type into ', () => {
     let fakeEventObject = {
       target: {
         //object must include ALL the props that we have added to the target
-        name: "comment",
-        value: "This is the test comment"
+        name: 'comment',
+        value: 'This is the test comment'
       }
     };
 
-    let textareas = wrapped.find("textarea");
-    textareas.simulate("change", fakeEventObject);
+    let textareas = wrapped.find('textarea');
+    textareas.simulate('change', fakeEventObject);
     textareas.update();
 
     // can no longer reference textareas as it is 'stale' -> need a fresh reference to the component for assertion
-    textareas = wrapped.find("textarea");
-    expect(textareas.prop("value")).toEqual("This is the test comment");
+    textareas = wrapped.find('textarea');
+    expect(textareas.prop('value')).toEqual('This is the test comment');
     // expect(wrapped.find("textarea").prop("value")).toEqual("This is the test comment");
   });
 });
